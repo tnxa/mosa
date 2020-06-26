ad0456ad0bccbaa28e3df163ee08f0658a8b5f19
tnxa <imtnxa@gmail.com>
2020-06-25 20:53:01 -0500

    update manifest name in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `mosa ui`,
+        short_name: `mosa`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
