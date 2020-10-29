7ed1b0b47262806b6a145ecd6b7fa35eee05ca67
tnxa <59325699+tnxa@users.noreply.github.com>
2020-10-28 22:25:47 -0500

    prototype random pattern mode (#15)

---
+++ src/config/strokes.js
@@ -0,0 +1,26 @@
+export const patterns = {
+  LongStrokes: {
+    name: 'Long Strokes',
+    stroke: {
+      center: 500,
+      min: 700,
+      max: 800,
+    },
+    angles: {
+      pitch: 100,
+      roll: 100,
+    },
+  },
+  ShortStrokes: {
+    name: 'Short Strokes',
+    stroke: {
+      center: 400,
+      min: 200,
+      max: 400,
+    },
+    angles: {
+      pitch: 200,
+      roll: 100,
+    },
+  },
+}
