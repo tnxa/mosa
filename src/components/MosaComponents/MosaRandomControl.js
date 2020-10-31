ec6bfd4c32a6d549815f02d805ac01c78226c878
tnxa <59325699+tnxa@users.noreply.github.com>
2020-10-31 02:39:48 -0500

    add prototype stroke functions (#18)

--- src/components/MosaComponents/MosaRandomControl.js
+++ src/components/MosaComponents/MosaRandomControl.js
@@ -9,99 +9,42 @@ import {
 } from '@material-ui/core'
 
 import { useInterval } from '../../hooks/useIntervalHook'
-import { arbitraryRandom, chooseRandom } from '../../utils/random'
+import { chooseRandom } from '../../utils/random'
 
-import { clampedNum } from '../../utils/clamp'
-
-import { patterns } from '../../config/strokes'
-
-const getStroke = (stroke, L0) => {
-  const strokeLength = arbitraryRandom(stroke.min, stroke.max)
-  return clampedNum(
-    L0 < stroke.center ? L0 + strokeLength : L0 - strokeLength,
-    0,
-    999
-  )
-}
-
-const getAngle = (angle, current) => {
-  const newAngle = arbitraryRandom(0, angle)
-  return clampedNum(
-    Math.random() > 0.3 ? 500 + newAngle : 500 - newAngle,
-    0,
-    999
-  )
-}
-
-const chooseNextDestination = (pattern, position) => {
-  return {
-    L0: getStroke(pattern.stroke, position.L0),
-    R1: getAngle(pattern.angles.roll, position.R1), // arbitraryRandom(0, 999),
-    R2: getAngle(pattern.angles.pitch, position.R2), // arbitraryRandom(0, 999),
-  }
-}
-
-// currently only supports OSR2+ axes // TODO: support more
-const calculateProfile = (position, destination, timer) => {
-  return {
-    dL0: (destination.L0 - position.L0) / timer,
-    dR1: (destination.R1 - position.R1) / timer,
-    dR2: (destination.R2 - position.R2) / timer,
-  }
-}
-
-// currently only supports OSR2+ axes // TODO: support more
-const calculateNextPosition = (position, profile, step) => {
-  return {
-    L0: position.L0 + profile.dL0 * step,
-    R1: position.R1 + profile.dR1 * step,
-    R2: position.R2 + profile.dR2 * step,
-  }
-}
+import { strokes } from '../../config/strokes'
 
 export const MosaRandomControl = props => {
   const { connected, target, commandRobot } = props
 
   const [running, setRunning] = useState(false)
 
-  const [timer, setTimer] = useState(500)
+  const [timer, setTimer] = useState(0) // start with no seconds on the clock
   const [step, setStep] = useState(50)
-  const [patternTimer, setPatternTimer] = useState([0.5, 3.0])
   const [randomness, setRandomness] = useState(30)
-  const [currentPattern, setCurrentPattern] = useState(chooseRandom(patterns))
+  const [strokeType, setStrokeType] = useState(chooseRandom(strokes))
+  const [stroke, setStroke] = useState(strokeType.getStroke(target, step))
+  const [strokeCounter, setStrokeCounter] = useState(0)
 
-  const [profile, setProfile] = useState({
-    dL0: 0,
-    dL1: 0,
-    dL2: 0,
-    dR0: 0,
-    dR1: 0,
-    dR2: 0,
-  })
-
-  // TODO: calculate sine targets in a more sophisticated way
   useInterval(() => {
     if (running && connected) {
       if (timer <= 0) {
-        // do we change pattern?
-        const pattern =
-          Math.random() < randomness / 100
-            ? chooseRandom(patterns)
-            : currentPattern
+        // do we change stroke?
+        const newStrokeType =
+          Math.random() * 100 < randomness ? chooseRandom(strokes) : strokeType
 
-        const newTimer =
-          timer +
-          arbitraryRandom(patternTimer[0] * 1000, patternTimer[1] * 1000)
-        const nextDestination = chooseNextDestination(pattern, target) // choose the next destination
-        const profile = calculateProfile(target, nextDestination, newTimer)
+        const stroke = newStrokeType.getStroke(target, step)
+        const newTimer = step * stroke.length // derive next timing from length of stroke
 
-        setCurrentPattern(pattern)
-        setProfile(profile)
+        setStrokeType(newStrokeType)
+        setStroke(stroke)
         setTimer(newTimer) // add time to the timer
+        setStrokeCounter(strokeCounter + 1)
       } else {
-        const nextPosition = calculateNextPosition(target, profile, step)
-        commandRobot(nextPosition, step / 1000)
-        setTimer(timer - step)
+        // execute the next stroke step
+        const destination = stroke[0]
+        commandRobot(destination, step / 1000)
+        setStroke(stroke.slice(1))
+        setTimer(timer - step) // subtract time from the timer
       }
     } // if not running & connected, no-op
   }, step) // next execution time will be `step` away
@@ -109,25 +52,15 @@ export const MosaRandomControl = props => {
   return (
     <Card>
       <CardContent>
-        <Typography variant="h5">Random: {currentPattern.name}</Typography>
+        <Typography variant="h5">Random: {strokeType.name}</Typography>
         <hr />
-        <Typography>Pattern Change: {randomness}%</Typography>
+        <Typography>Change Stroke: {randomness}%</Typography>
         <Slider
           value={randomness}
           min={0}
           max={100}
           valueLabelDisplay={'auto'}
           onChange={(e, value) => setRandomness(value)}
-        />
-        <Typography>
-          Stroke Time ({patternTimer[0]} - {patternTimer[1]} seconds)
-        </Typography>
-        <Slider
-          value={patternTimer}
-          min={0.5}
-          max={10}
-          valueLabelDisplay={'auto'}
-          onChange={(e, values) => setPatternTimer(values)}
         />
         <Typography>Update Frequency</Typography>
         <Slider
@@ -139,6 +72,7 @@ export const MosaRandomControl = props => {
           valueLabelDisplay={'auto'}
           onChange={(e, value) => setStep(value)}
           marks={[{ value: 50, label: '50ms/step' }]}
+          disabled={running}
         />
       </CardContent>
       <CardActions>
@@ -151,8 +85,9 @@ export const MosaRandomControl = props => {
           {running ? 'STOP' : 'START'}
         </Button>
         <Typography>
-          Stroke Timer: {(timer >= 0 ? timer / 1000 : 0).toFixed(1)}s
+          Next: {(timer >= 0 ? timer / 1000 : 0).toFixed(1)}s
         </Typography>
+        <Typography>-- Stroke #{strokeCounter}</Typography>
       </CardActions>
     </Card>
   )
--- src/config/strokes.js
+++ src/config/strokes.js
@@ -1,26 +1,180 @@
-export const patterns = {
-  LongStrokes: {
-    name: 'Long Strokes',
-    stroke: {
-      center: 500,
-      min: 700,
-      max: 800,
-    },
-    angles: {
-      pitch: 100,
-      roll: 100,
+import { clampedNum } from '../utils/clamp'
+
+// each element of `strokes` should have a `name` and a `getStroke(position, step)` function, at a minimum.
+// the `getStroke` function should, given a current position and step time, return an array of commands
+// commands returned should be in the format [ {}, {}, {}, ... ]
+// individual commands should be in the format { L0: #, L1: #, L2: #, R0: #, R1: #, R2: # };
+// where L0-L2, R0-R2 are the target position in tcode axes.
+
+// todo: develop more stroke types and parameterize them // #helpwanted
+
+export const strokes = {
+  ShortStrokes: {
+    name: 'Short Stroke',
+    getStroke: (position, step) => {
+      const strokeTime = 800 // .8 seconds
+      const distanceFactor = 0.5
+
+      const steps = strokeTime / step
+
+      let stroke = [],
+        pL0 = position.L0, // previous L0
+        pR1 = position.R1, // previous L1
+        pR2 = position.R2 // previous L2
+
+      const direction = pL0 < 500 ? 1 : -1 // if in bottom half, we go up; if in top half, we go down
+      const possibleStrokeLength = Math.max(pL0, 1000 - pL0) * distanceFactor // current to bottom, or current to top
+
+      for (let i = 1; i <= steps; i++) {
+        // choose next points
+        const nL0 = clampedNum(
+            pL0 + (direction * possibleStrokeLength) / steps,
+            0,
+            1000
+          ),
+          nR1 = (pR1 + 500) / 2, // eventually move to middle
+          nR2 = (pR2 + 500) / 2 // eventually move to middle
+
+        // append next to stroke points
+        stroke.push({
+          L0: nL0,
+          R1: nR1,
+          R2: nR2,
+        })
+
+        // for next iteration, new points become previous points
+        pL0 = nL0
+        pR1 = nR1
+        pR2 = nR2
+      }
+
+      return stroke
     },
   },
-  ShortStrokes: {
-    name: 'Short Strokes',
-    stroke: {
-      center: 400,
-      min: 200,
-      max: 400,
+  LongStrokes: {
+    name: 'Long Stroke',
+    getStroke: (position, step) => {
+      const strokeTime = 800 // .8 seconds
+      const distanceFactor = 0.9
+
+      const steps = strokeTime / step
+
+      let stroke = [],
+        pL0 = position.L0, // previous L0
+        pR1 = position.R1, // previous L1
+        pR2 = position.R2 // previous L2
+
+      const direction = pL0 < 500 ? 1 : -1 // if in bottom half, we go up; if in top half, we go down
+      const possibleStrokeLength = Math.max(pL0, 1000 - pL0) * distanceFactor // current to bottom, or current to top
+
+      for (let i = 1; i <= steps; i++) {
+        // choose next points
+        const nL0 = clampedNum(
+            pL0 + (direction * possibleStrokeLength) / steps,
+            0,
+            1000
+          ),
+          nR1 = (pR1 + 500) / 2, // eventually move to middle
+          nR2 = (pR2 + 500) / 2 // eventually move to middle
+
+        // append next to stroke points
+        stroke.push({
+          L0: nL0,
+          R1: nR1,
+          R2: nR2,
+        })
+
+        // for next iteration, new points become previous points
+        pL0 = nL0
+        pR1 = nR1
+        pR2 = nR2
+      }
+
+      return stroke
     },
-    angles: {
-      pitch: 200,
-      roll: 100,
+  },
+  LongJerky: {
+    name: 'Long Jerk',
+    getStroke: (position, step) => {
+      // const strokeTime = 2000 // 2 seconds
+      const maxPercentTravel = 0.75
+      const jerk = 0.01 // todo: leverage some function of this to gently accel/decel
+
+      // const steps = strokeTime / step
+
+      const oL0 = position.L0,
+        oR1 = position.R1,
+        oR2 = position.R2
+
+      let stroke = [],
+        acceleration = 0,
+        velocity = 0,
+        pL0 = oL0, // previous L0 starts as original L0
+        pR1 = oR1, // previous L1 starts as original L1
+        pR2 = oR2 // previous L2 starts as original L2
+
+      const direction = pL0 < 500 ? 1 : -1 // if in bottom half, we go up; if in top half, we go down
+      const possibleStrokeLength = Math.max(pL0, 1000 - pL0) * maxPercentTravel // current to bottom, or current to top
+      const fL0 = pL0 + direction * possibleStrokeLength // final L0
+
+      // condition based on direction; loop until we pass destination
+      while (direction > 0 ? pL0 < fL0 : pL0 > fL0) {
+        acceleration += direction * jerk * step
+        velocity += acceleration + (1 / 2) * direction * jerk * step
+
+        const positional =
+          velocity + (1 / 2) * acceleration + (1 / 6) * direction * jerk
+
+        const nL0 = clampedNum((pL0 += positional), 0, 1000),
+          nR1 = (pR1 + 500) / 2, // eventually move to middle
+          nR2 = (pR2 + 500) / 2 // eventually move to middle
+
+        stroke.push({
+          L0: nL0,
+          R1: nR1,
+          R2: nR2,
+        })
+
+        // for next iteration, new points become previous points
+        pL0 = nL0
+        pR1 = nR1
+        pR2 = nR2
+      }
+      return stroke
+    },
+  },
+  Orbit: {
+    name: 'Orbit',
+    getStroke: (position, step) => {
+      const strokeTime = 2000 // 2 seconds
+      const steps = strokeTime / step
+      const oL0 = position.L0,
+        oR1 = position.R1,
+        oR2 = position.R2
+
+      let stroke = [],
+        pL0 = oL0, // previous L0 starts as original L0
+        pR1 = oR1, // previous L1 starts as original L1
+        pR2 = oR2 // previous L2 starts as original L2
+
+      for (let i = 1; i <= steps; i++) {
+        // choose next points
+        const nL0 = pL0 + 5 * Math.cos((i / steps) * Math.PI),
+          nR1 = pR1 + 20 * Math.cos((i / steps) * 2 * Math.PI),
+          nR2 = pR2 + 15 * Math.sin((i / steps) * 2 * Math.PI) // can we find a way to center this?
+
+        // append next to stroke points
+        stroke.push({
+          L0: nL0,
+          R1: nR1,
+          R2: nR2,
+        })
+        // for next iteration, new points become previous points
+        pL0 = nL0
+        pR1 = nR1
+        pR2 = nR2
+      }
+      return stroke
     },
   },
 }
