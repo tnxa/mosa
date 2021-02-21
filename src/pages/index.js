16e6fd1cf1414a618e67dbbeda9fa413d6dd3dea
tnxa <imtnxa@gmail.com>
2021-02-21 03:15:33 -0600

    create stored settings context for output range

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,12 @@ import SEO from '../components/seo'
 import { Card, Typography, CardContent, Grid, Divider } from '@material-ui/core'
 import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab'
 
-import { defaultTarget, defaultRange } from '../config/defaults'
+import { defaultRange, defaultTarget } from '../config/defaults'
 
 import { useSerial } from '../hooks/useSerialHook'
 import { scaleAxes, constructTCodeCommand } from '../utils/tcode'
+
+import { SettingsContext } from '../../gatsby-browser'
 
 import MosaOutputRangeControl from '../components/MosaComponents/MosaOutputRangeControl'
 import MosaMotionControl from '../components/MosaComponents/MosaMotionControl'
@@ -27,7 +29,6 @@ const IndexPage = () => {
   const [connected, setConnected] = useState(false)
 
   const [target, setTarget] = useState(defaultTarget)
-  const [outputRange, setOutputRange] = useState(defaultRange)
 
   const [inputMethod, setSelectedInputMethod] = useState('web')
   const [outputMethod, setSelectedOutputMethod] = useState()
@@ -37,10 +38,10 @@ const IndexPage = () => {
   }
 
   // todo: make this better
-  const handleOutputMethodChange = (event, newOutputMethod) => {
+  const handleOutputMethodChange = async (event, newOutputMethod) => {
     if (connected) {
-      handleDisconnectFromSerial() // eventually, all methods
-      handleDisconnectFromVisualization()
+      await handleDisconnectFromSerial() // eventually, all methods
+      await handleDisconnectFromVisualization()
     }
 
     switch (newOutputMethod) {
@@ -74,7 +75,7 @@ const IndexPage = () => {
     }
   }
   const handleDisconnectFromSerial = async () => {
-    commandRobot(defaultTarget, 1)
+    commandRobot(defaultTarget, 1, defaultRange)
     await disconnectFromSerial()
     setConnected(false)
   }
@@ -88,13 +89,13 @@ const IndexPage = () => {
     setConnected(false)
   }
 
-  const commandRobot = (destination, interval) => {
+  const commandRobot = (destination, interval, settings) => {
     // persist target to state
     const newTarget = { ...target, ...destination }
     setTarget(newTarget)
 
     // tell the robot what to do
-    const scaledDestination = scaleAxes(newTarget, outputRange)
+    const scaledDestination = scaleAxes(newTarget, settings)
     const command = constructTCodeCommand(scaledDestination, interval)
     switch (outputMethod) {
       case 'serial':
@@ -109,98 +110,119 @@ const IndexPage = () => {
   }
 
   return (
-    <Layout>
-      <SEO title="Controls" />
-      <Grid container spacing={2} justify="center">
-        <Grid item xs={12} md={4} lg={3}>
-          <Card>
-            <CardContent>
-              <Typography>Input: {!inputMethod && 'none selected'} </Typography>
-              <ToggleButtonGroup
-                value={inputMethod}
-                exclusive
-                onChange={handleInputMethodChange}
-              >
-                <ToggleButton value="web">WEB</ToggleButton>
-                <ToggleButton value="remote" disabled>
-                  REMOTE
-                </ToggleButton>
-              </ToggleButtonGroup>
-              <br />
-              <br />
-              <Typography>
-                Output: {!outputMethod && 'none selected'}
-              </Typography>
-              <ToggleButtonGroup
-                value={outputMethod}
-                exclusive
-                onChange={handleOutputMethodChange}
-              >
-                {isSerialAvailable && (
-                  <ToggleButton value="serial">SERIAL</ToggleButton>
-                )}
-                <ToggleButton value="visualizer">SR-VIS</ToggleButton>
-              </ToggleButtonGroup>
-              <br />
-              <br />
-              <Typography variant="caption">(more I/O coming soon)</Typography>
-              {!isSerialAvailable && ( // if serial not available, explain
-                <>
+    <SettingsContext.Consumer>
+      {({ settings, updateSettings }) => (
+        <Layout>
+          <SEO title="Controls" />
+          <Grid container spacing={2} justify="center">
+            <Grid item xs={12} md={4} lg={3}>
+              <Card>
+                <CardContent>
+                  <Typography>
+                    Input: {!inputMethod && 'none selected'}
+                  </Typography>
+                  <ToggleButtonGroup
+                    value={inputMethod}
+                    exclusive
+                    onChange={handleInputMethodChange}
+                  >
+                    <ToggleButton value="web">WEB</ToggleButton>
+                    <ToggleButton value="remote" disabled>
+                      REMOTE
+                    </ToggleButton>
+                  </ToggleButtonGroup>
+                  <br />
                   <br />
                   <Typography>
-                    Could not detect serial capabilities. Please use the latest
-                    version of Chrome, open <code>chrome://flags</code>, and set
-                    <code>#enable-experimental-web-platform-features</code>{' '}
-                    (note that these are experimental features, use at your own
-                    risk, etc etc)
+                    Output: {!outputMethod && 'none selected'}
                   </Typography>
-                </>
-              )}
-            </CardContent>
-          </Card>
-          <hr />
-          <MosaVisualizer target={target} />
-          <hr />
-          <MosaOutputRangeControl
-            outputRange={outputRange}
-            setOutputRange={setOutputRange}
-          />
-        </Grid>
-        <Grid item xs={12} md={4} lg={5}>
-          <MosaMotionControl
-            connected={connected}
-            target={target}
-            commandRobot={commandRobot}
-          />
-          <hr />
-          <MosaVibeControl
-            connected={connected}
-            target={target}
-            commandRobot={commandRobot}
-          />
-        </Grid>
-        <Grid item xs={12} md={4} lg={4}>
-          <MosaPlanarControl
-            connected={connected}
-            commandRobot={commandRobot}
-          />
-          <hr />
-          <MosaRandomControl
-            connected={connected}
-            target={target}
-            commandRobot={commandRobot}
-          />
-          <hr />
-          <MosaSineControl
-            connected={connected}
-            target={target}
-            commandRobot={commandRobot}
-          />
-        </Grid>
-      </Grid>
-      &nbsp;
-      <Divider />
-    </Layout>
+                  <ToggleButtonGroup
+                    value={outputMethod}
+                    exclusive
+                    onChange={handleOutputMethodChange}
+                  >
+                    {isSerialAvailable && (
+                      <ToggleButton value="serial">SERIAL</ToggleButton>
+                    )}
+                    <ToggleButton value="visualizer">SR-VIS</ToggleButton>
+                  </ToggleButtonGroup>
+                  <br />
+                  <br />
+                  <Typography variant="caption">
+                    (more I/O coming soon)
+                  </Typography>
+                  {!isSerialAvailable && ( // if serial not available, explain
+                    <>
+                      <br />
+                      <Typography>
+                        Could not detect serial capabilities. Please use the
+                        latest version of Chrome, open{' '}
+                        <code>chrome://flags</code>, and set
+                        <code>
+                          #enable-experimental-web-platform-features
+                        </code>{' '}
+                        (note that these are experimental features, use at your
+                        own risk, etc etc)
+                      </Typography>
+                    </>
+                  )}
+                </CardContent>
+              </Card>
+              <hr />
+              <MosaVisualizer target={target} />
+              <hr />
+              <MosaOutputRangeControl
+                settings={settings}
+                updateSettings={updateSettings}
+              />
+            </Grid>
+            <Grid item xs={12} md={4} lg={5}>
+              <MosaMotionControl
+                connected={connected}
+                target={target}
+                commandRobot={(destination, interval) =>
+                  commandRobot(destination, interval, settings)
+                }
+              />
+              <hr />
+              <MosaVibeControl
+                connected={connected}
+                target={target}
+                commandRobot={(destination, interval) =>
+                  commandRobot(destination, interval, settings)
+                }
+              />
+            </Grid>
+            <Grid item xs={12} md={4} lg={4}>
+              <MosaPlanarControl
+                connected={connected}
+                commandRobot={(destination, interval) =>
+                  commandRobot(destination, interval, settings)
+                }
+              />
+              <hr />
+              <MosaRandomControl
+                connected={connected}
+                target={target}
+                commandRobot={(destination, interval) =>
+                  commandRobot(destination, interval, settings)
+                }
+              />
+              <hr />
+              <MosaSineControl
+                connected={connected}
+                target={target}
+                commandRobot={(destination, interval) =>
+                  commandRobot(destination, interval, settings)
+                }
+              />
+            </Grid>
+          </Grid>
+          &nbsp;
+          <Divider />
+        </Layout>
+      )}
+    </SettingsContext.Consumer>
   )
 }
 
